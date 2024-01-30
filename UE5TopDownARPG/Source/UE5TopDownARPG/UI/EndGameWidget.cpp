212e5aba8aef636109c1d08685acef2eceefa6e0
SupPanda9 <nikeikata@gmail.com>
2024-01-30 17:48:35 +0200

    Added logic to Play and Quit buttons.

--- UE5TopDownARPG/Source/UE5TopDownARPG/UI/EndGameWidget.cpp
+++ UE5TopDownARPG/Source/UE5TopDownARPG/UI/EndGameWidget.cpp
@@ -1,5 +1,36 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "EndGameWidget.h"
+#include "Components/Button.h"
+#include "Kismet/GameplayStatics.h"
+#include "Kismet/KismetSystemLibrary.h"
 
+bool UEndGameWidget::Initialize()
+{
+	// Call the base class Initialize function
+	bool success = Super::Initialize();
+	if (!success) return false;
+
+	// Check if the buttons are valid
+	if (!ensure(PlayButton != nullptr)) return false;
+	if (!ensure(QuitButton != nullptr)) return false;
+
+	// Bind the OnClicked events of the buttons to the functions
+	PlayButton->OnClicked.AddDynamic(this, &UEndGameWidget::OnPlayClicked);
+	QuitButton->OnClicked.AddDynamic(this, &UEndGameWidget::OnQuitClicked);
+
+	// Return true if everything is successful
+	return true;
+}
+
+void UEndGameWidget::OnPlayClicked()
+{
+	UGameplayStatics::OpenLevel(GetWorld(), "GameLevel");
+
+	SetVisibility(ESlateVisibility::Collapsed);
+}
+
+void UEndGameWidget::OnQuitClicked()
+{
+	UKismetSystemLibrary::QuitGame(GetWorld(), GetOwningPlayer(), EQuitPreference::Quit, false);
+}
--- UE5TopDownARPG/Source/UE5TopDownARPG/UI/EndGameWidget.h
+++ UE5TopDownARPG/Source/UE5TopDownARPG/UI/EndGameWidget.h
@@ -13,5 +13,23 @@ UCLASS()
 class UE5TOPDOWNARPG_API UEndGameWidget : public UUserWidget
 {
 	GENERATED_BODY()
-	
+
+public:
+	// Declare two UFUNCTIONs with the BlueprintCallable specifier
+	UFUNCTION(BlueprintCallable)
+	void OnPlayClicked();
+
+	UFUNCTION(BlueprintCallable)
+	void OnQuitClicked();
+
+protected:
+	// Override the Initialize function
+	virtual bool Initialize() override;
+
+	// Declare two UButton pointers and use the BindWidget meta specifier
+	UPROPERTY(meta = (BindWidget))
+	class UButton* PlayButton;
+
+	UPROPERTY(meta = (BindWidget))
+	class UButton* QuitButton;
 };
